5aca36cab03205756f60b5a447aff19bb0d9ca2f
kielou3 <kielou3@gmail.com>
2022-12-20 01:13:29 +0800

    yey

--- lib/view/household_details_page.dart
+++ lib/view/household_details_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:uhc_geoteagging/model/household.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Householddetails extends StatefulWidget {
   final Household? household;
@@ -11,28 +13,48 @@ class Householddetails extends StatefulWidget {
 }
 
 class _HouseholddetailsState extends State<Householddetails> {
-  Icon icon = Icon(
+  Icon icon = const Icon(
     Icons.location_off,
     color: Colors.red,
   );
-  void initState() {
-    super.initState();
-    debugPrint(widget.household!.longitude);
-  }
+  // void initState() {
+  //   super.initState();
+  //   debugPrint(widget.household!.longitude);
+  // }
 
   checkLong() async {
     if (widget.household!.longitude != null) {
-      icon = Icon(
+      icon = const Icon(
         Icons.location_on,
         color: Colors.blue,
       );
     }
   }
 
-  checkLat() {
-    if (widget.household?.longitude == null) {
-      return '';
+  void checkLat() async {
+    debugPrint('asda');
+    await Geolocator.checkPermission();
+    await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    var uri = 'http://uhcsocot20222-001-site1.dtempurl.com/api/UpdateLocation';
+    Response response = await post(Uri.parse(uri), body: {
+      'Id': widget.household?.id.toString(),
+      'Latitude': position.latitude.toString(),
+      'Longitude': position.longitude.toString(),
+    });
+    if (response.statusCode == 200) {
+      setState(() {
+        widget.household?.latitude = position.latitude;
+        widget.household?.longitude = position.longitude;
+      });
+      print(position.latitude);
+      debugPrint('None');
+    } else {
+      debugPrint('error');
     }
+    debugPrint('None');
   }
 
   @override
@@ -59,9 +81,15 @@ class _HouseholddetailsState extends State<Householddetails> {
             ),
           ),
           ListTile(
-            title: Text(widget.household?.longitude ?? 'Not Available'),
+            title:
+                Text(widget.household?.longitude.toString() ?? 'Not Available'),
             leading: icon,
           ),
+          ElevatedButton(
+              onPressed: () {
+                checkLat();
+              },
+              child: Text('Set Location'))
         ]),
       ),
     );
