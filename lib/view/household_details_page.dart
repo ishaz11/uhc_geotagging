3ebe5fa7b9b7dae675fd067ed30776f675936cff
kielou3 <kielou3@gmail.com>
2022-12-20 11:45:21 +0800

    Update icon when location is not null

--- lib/view/household_details_page.dart
+++ lib/view/household_details_page.dart
@@ -31,10 +31,49 @@ class _HouseholddetailsState extends State<Householddetails> {
     }
   }
 
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
   void checkLat() async {
-    debugPrint('asda');
-    await Geolocator.checkPermission();
-    await Geolocator.requestPermission();
+    _determinePosition();
+    // await Geolocator.checkPermission();
+    // await Geolocator.requestPermission();
+
+    // debugPrint(pos.toString());
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
 
@@ -50,11 +89,7 @@ class _HouseholddetailsState extends State<Householddetails> {
         widget.household?.longitude = position.longitude;
       });
       print(position.latitude);
-      debugPrint('None');
-    } else {
-      debugPrint('error');
-    }
-    debugPrint('None');
+    } else {}
   }
 
   @override
@@ -68,14 +103,14 @@ class _HouseholddetailsState extends State<Householddetails> {
         child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
           ListTile(
             title: Text(widget.household!.headofFamily.toString()),
-            leading: Icon(
+            leading: const Icon(
               Icons.man,
               color: Colors.green,
             ),
           ),
           ListTile(
             title: Text(widget.household!.address.toString()),
-            leading: Icon(
+            leading: const Icon(
               Icons.house_rounded,
               color: Colors.blue,
             ),
@@ -89,7 +124,7 @@ class _HouseholddetailsState extends State<Householddetails> {
               onPressed: () {
                 checkLat();
               },
-              child: Text('Set Location'))
+              child: const Text('Update Location'))
         ]),
       ),
     );
